ec03335c963974755d83dff6a8d4631e71274cb2
bRad-95 <bryantramirez@ac.c4q.nyc>
2017-11-21 19:46:06 -0500

    part01 commit

--- app/src/main/java/nyc/c4q/whatismyaddress/MainActivity.java
+++ app/src/main/java/nyc/c4q/whatismyaddress/MainActivity.java
@@ -1,13 +1,52 @@
 package nyc.c4q.whatismyaddress;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREFS_KEY = "address_shared_prefrences";
+    EditText editText;
+    Button button1;
+    Button button2;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.email_edittext);
+        button1 = (Button) findViewById(R.id.button01);
+        button2 = (Button) findViewById(R.id.button02);
+
+        preferences = getApplicationContext().getSharedPreferences(SHARED_PREFS_KEY,MODE_PRIVATE);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+
+             String email = editText.getText().toString();
+             editor.putString("email" , email);
+             editor.apply();
+             editor.commit();
+             editText.setText("");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RecyclerActivity.class);
+                intent.putExtra("sharedprefs", SHARED_PREFS_KEY);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/nyc/c4q/whatismyaddress/RecyclerActivity.java
@@ -0,0 +1,8 @@
+package nyc.c4q.whatismyaddress;
+
+/**
+ * Created by c4q on 11/21/17.
+ */
+
+public class RecyclerActivity {
+}
